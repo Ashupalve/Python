babfb47f449a7f8a032547e5599361a636091e93
Ashwed Palve <ashwedp@gmail.com>
2026-09-26 22:11:56 +0530

    Q2 done of CH12 PS

--- Basics/CH12_PS/02.py
+++ Basics/CH12_PS/02.py
@@ -1,2 +1,10 @@
 # 2. Write a program to print third, fifth and seventh element from a list using enumerate
 # function.
+
+
+l = [12,1,25,36,47,58,65,32,15,35,11,8]
+print(type(l))
+
+for i ,item in enumerate(l):
+    if i == 2 or i == 4 or i == 6 :
+        print(item)
